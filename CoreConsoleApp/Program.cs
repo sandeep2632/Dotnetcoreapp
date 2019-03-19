9fec8845b4f7a18f9c04ae77ccfe44dd61a100cc
Sandeep Singh <sandeep.singh02@nagarro.com>
2019-03-19 15:47:57 +0530

    third commit

---
+++ CoreConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace CoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coll = new string[] { "Small", "Medium", "Large" };
+
+            string output = JsonConvert.SerializeObject(coll);
+            Console.WriteLine("Hello World!"+ output);
+        }
+    }
+}
